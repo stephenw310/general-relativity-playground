53bb08dfdeb8c3825afcdaf7716c2dc3dbf18065
Chao Wu <magicchao1989@gmail.com>
2025-06-19 13:15:40 -0700

    Add FPS counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stats } from "@react-three/drei";
 import { Leva } from "leva";
 import { CurvedGrid } from "@/components/curved-grid";
 import { MassHandles } from "@/components/mass-handles";
@@ -127,6 +127,8 @@ export default function Home() {
         />
 
         <MassHandles masses={masses} />
+
+        <Stats />
       </Canvas>
     </div>
   );
